d7ae8989e2499b3ecd6e0daabca5a114eaf063af
John Hewitt <86196229+growcacti@users.noreply.github.com>
2023-11-11 03:34:01 -0700

    Add files via upload

---
+++ comboboxes.py
@@ -0,0 +1,53 @@
+r0c1 = ttk.Combobox(
+    f1,
+    values=listedstuff,
+)
+r0c1.grid(row=0, column=1)
+
+r1c1 = ttk.Combobox(
+    f1,
+    values=listedstuff,
+)
+r1c1.grid(row=1, column=1)
+
+r2c1 = ttk.Combobox(
+    f1,
+    values=listedstuff,
+)
+r2c1.grid(row=2, column=1)
+r0c2 = ttk.Combobox(
+    f1,
+    values=listedstuff,
+)
+r0c2.grid(row=0, column=2)
+
+r1c2 = ttk.Combobox(
+    f1,
+    values=listedstuff,
+)
+r1c2.grid(row=1, column=2)
+r0c3 = ttk.Combobox(
+    f1,
+    values=listedstuff,
+)
+r0c3.grid(row=0, column=3)
+
+r1c3 = ttk.Combobox(
+    f1,
+    values=listedstuff,
+)
+r1c3.grid(row=1, column=3)
+
+
+
+r0c4 = ttk.Combobox(
+    f1,
+    values=listedstuff,
+)
+r0c4.grid(row=0, column=4)
+
+r1c4 = ttk.Combobox(
+    f1,
+    values=listedstuff,
+)
+r1c4.grid(row=1, column=4)
